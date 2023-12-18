7528c9fe23f7cec6596bf4fb4a787466b85262e4
HiPark <youuy28@gmail.com>
2023-12-18 20:52:27 +0900

    init commit

---
+++ Question/20231212-easy.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/two-sum/
+
+class Solution(object):
+    # def twoSum(self, nums, target):
+    #     """
+    #     :type nums: List[int]
+    #     :type target: int
+    #     :rtype: List[int]
+    #     """
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
