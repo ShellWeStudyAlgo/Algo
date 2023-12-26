bc79fdf185b3d565dcb2395f29bc90b6bf0b7f83
JongIn Yoon <97035666+bellpp93@users.noreply.github.com>
2023-12-27 00:24:02 +0900

    Update yoon-20231212.py

--- Yoon/yoon-20231212.py
+++ Yoon/yoon-20231212.py
@@ -5,7 +5,7 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
+        for i in range(len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
