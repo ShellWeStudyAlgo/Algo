e3a93af0ad1b67c2417787df35008c4d7d153ea3
JongIn Yoon <97035666+bellpp93@users.noreply.github.com>
2023-12-28 01:02:46 +0900

    Update yoon-20231212.py

--- Yoon/yoon-20231212.py
+++ Yoon/yoon-20231212.py
@@ -5,7 +5,19 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+        # for i in range(len(nums)-1):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
+
+        # n² => Refactoring(Use Dictionary)
+        num_dict = {}
+
+        for idx in range(len(nums)):
+            num = nums[idx]
+            sub_value = target - num
+
+            if sub_value in num_dict:
+                return [num_dict[sub_value], idx]
+
+            num_dict[num] = idx
